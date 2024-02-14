12a95ed1f4633905d3ec08643bec76daf544edfe
Sikhuphela <aphiwe.sikhuphela@younglings.africa>
2024-02-14 10:19:32 +0200

    Added a rectangle to the contact form

--- src/Pages/Contacts.js
+++ src/Pages/Contacts.js
@@ -6,9 +6,8 @@ function Contacts() {
     return (
         <div className='contact'>
             <h3>Contact Me</h3>
-            
-            {/* Google Maps */}
             <div className='rectangle'>
+          
                 <iframe 
                     title="Google Maps"
                     src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d165800.04030341068!2d18.4240553!3d-33.9248685!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x1dcc500f8826eed7%3A0x687fe1fc2828aa87!2sCape%20Town!5e0!3m2!1sen!2sza!4v1644720678477!5m2!1sen!2sza"
@@ -19,16 +18,13 @@ function Contacts() {
                     loading="lazy"
                 ></iframe>
             </div>
-            
-            {/* Contact form */}
             <div className='group'>
+            <div className='rectangle-5'>
                 <div className='overlap'>
                     <div className='text-wrapper'>Message</div>
                     <div className='div'>Email</div>
                     <div className='text-wrapper-2'>Name</div>
                     <div className='text-wrapper-3'>Subject</div>
-                    
-                    {/* Submit button */}
                     <div className='rectangle-1'>
                         <div className='rectangle-2'>
                             <div className='rectangle-3'>
@@ -50,6 +46,7 @@ function Contacts() {
                     </div>
                 </div>
             </div>
+        </div>
         </div>
     );
 }
