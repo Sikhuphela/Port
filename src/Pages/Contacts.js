ee815696883bf40b2d6350beabb55b96c1d3e16e
Sikhuphela <aphiwe.sikhuphela@younglings.africa>
2024-02-14 11:49:33 +0200

    added a button hover

--- src/Pages/Contacts.js
+++ src/Pages/Contacts.js
@@ -4,10 +4,10 @@ import { FaGithub, FaDiscord, FaLinkedin } from 'react-icons/fa';
 
 function Contacts() {
     return (
-        <div className='contact'>
+        <div className='contact-container'>
             <h3>Contact Me</h3>
+            <div className='contact'>
             <div className='rectangle'>
-          
                 <iframe 
                     title="Google Maps"
                     src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d165800.04030341068!2d18.4240553!3d-33.9248685!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x1dcc500f8826eed7%3A0x687fe1fc2828aa87!2sCape%20Town!5e0!3m2!1sen!2sza!4v1644720678477!5m2!1sen!2sza"
@@ -19,24 +19,25 @@ function Contacts() {
                 ></iframe>
             </div>
             <div className='group'>
-            <div className='rectangle-5'>
-                <div className='overlap'>
-                    <div className='text-wrapper'>Message</div>
-                    <div className='div'>Email</div>
-                    <div className='text-wrapper-2'>Name</div>
-                    <div className='text-wrapper-3'>Subject</div>
-                    <div className='rectangle-1'>
-                        <div className='rectangle-2'>
-                            <div className='rectangle-3'>
-                                <div className='rectangle-4'>
-                                    <div className='group-wrapper'>
-                                        <div className='overlap-group-wrapper'>
-                                            <div className='overlap-group'>
-                                                <div className='group-2'>
-                                                <div className='div-wrapper'>
-                                                <button className='submit-btn'>Submit</button>
-                                            </div>
-                                            
+            <form>
+                <div className='rectangle-5'>
+                    <div className='overlap'>
+                        <div className='text-wrapper'>Message</div>
+                        <div className='div'>Email</div>
+                        <div className='text-wrapper-2'>Name</div>
+                        <div className='text-wrapper-3'>Subject</div>
+                        <div className='rectangle-1'>
+                            <div className='rectangle-2'>
+                                <div className='rectangle-3'>
+                                    <div className='rectangle-4'>
+                                        <div className='group-wrapper'>
+                                            <div className='overlap-group-wrapper'>
+                                                <div className='overlap-group'>
+                                                    <div className='group-2'>
+                                                        <div className='div-wrapper'>
+                                                            <button type="submit" className='submit-btn'>Submit</button>
+                                                        </div>
+                                                    </div>
                                                 </div>
                                             </div>
                                         </div>
@@ -46,9 +47,12 @@ function Contacts() {
                         </div>
                     </div>
                 </div>
-            </div>
+            </form>
+        </div>
+        
         </div>
         </div>
+    
     );
 }
 
