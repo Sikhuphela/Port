7f6c0ab8b2cf9b3ce3a131f883fd6111b504e069
Sikhuphela <aphiwe.sikhuphela@younglings.africa>
2024-02-15 11:20:36 +0200

    Added my first portfolio image

--- src/Pages/Projects.js
+++ src/Pages/Projects.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import BankingApp from '../../src/components/Images/bankApp.PNG';
+import Portfolio from '../../src/components/Images/portfolio.png';
 import './Projects.css';
 import { FaGithub } from 'react-icons/fa';
 
@@ -10,19 +11,30 @@ function Projects() {
       <div className='box'>
         <div className='group'>
           <img className='imag' alt='BankApp' src={BankingApp} />
-          <div className='text-wrapper'>BankApp</div>
+          <div className='text-wrapper'>Banking App</div>
           <div className="work">
             <div className="layer">
-              <p>User-friendly online banking application designed for easy access  for financial
-               management using Python.Users register receive number and PIN for secure access.
-              The application allows users to check balance, make secure deposits,and
-               conveniently withdraw funds, ensuring a safe and efficient banking experience.</p>
+              <p>User-friendly online banking application designed for easy access for financial management using Python. Users register receive number and PIN for secure access. The application allows users to check balance, make secure deposits, and conveniently withdraw funds, ensuring a safe and efficient banking experience.</p>
               <a href="https://github.com/Sikhuphela/Banking-App.git">
                 <FaGithub /> {/* GitHub icon */}
               </a>
             </div>
           </div>
         </div>
+
+        <div className='group-2'>
+  <img className='photo' alt='Portfolio' src={Portfolio} />
+  <div className='text-wrapper-1'>My First Portfolio</div>
+  <div className="work-1">
+    <div className="layer-1">
+      <p>User-friendly online portfolio showcasing my projects and skills. Designed with HTML, CSS, and JavaScript. Includes project descriptions, links to GitHub repositories, and contact information.</p>
+      <a href="https://github.com/Sikhuphela/Portfolio.git">
+        <FaGithub /> {/* GitHub icon */}
+      </a>
+    </div>
+  </div>
+</div>
+
       </div>
     </div>
   );
