11a7dbf291b52aeed44269e9494dbe08704b17eb
Sikhuphela <aphiwe.sikhuphela@younglings.africa>
2024-02-15 14:21:56 +0200

    added the links to github my projects

--- src/Pages/Projects.js
+++ src/Pages/Projects.js
@@ -11,6 +11,8 @@ function Projects() {
     <div className='projects'>
       <h3>Projects</h3>
       <div className='box'>
+
+        {/* Banking App */}
         <div className='group'>
           <img className='imag' alt='BankApp' src={BankingApp} />
           <div className='text-wrapper'>Banking App</div>
@@ -24,43 +26,47 @@ function Projects() {
           </div>
         </div>
 
+        {/* My First Portfolio */}
         <div className='group-2'>
-  <img className='photo' alt='Portfolio' src={Portfolio} />
-  <div className='text-wrapper-1'>My First Portfolio</div>
-  <div className="work-1">
-    <div className="layer-1">
-      <p>User-friendly online portfolio showcasing my projects and skills. Designed with HTML, CSS, and JavaScript. Includes project descriptions, links to GitHub repositories, and contact information.</p>
-      <a href="https://github.com/Sikhuphela/Portfolio.git">
-        <FaGithub /> {/* GitHub icon */}
-      </a>
-    </div>
-  </div>
-</div>
-<div className='calculator-3'>
-  <img className='calculator' alt='fac' src={FAC} />
-  <div className='text-wrapper-2'>Financial calculator</div>
-  <div className="work-2">
-    <div className="layer-2">
-      <p>User-friendly description of project 3.</p>
-      <a href="https://github.com/yourusername/project3.git">
-        <FaGithub /> 
-      </a>
-    </div>
-  </div>
-</div>
+          <img className='photo' alt='Portfolio' src={Portfolio} />
+          <div className='text-wrapper-1'>My First Portfolio</div>
+          <div className="work-1">
+            <div className="layer-1">
+              <p>User-friendly online portfolio showcasing my projects and skills. Designed with HTML, CSS, and JavaScript. Includes project descriptions, links to GitHub repositories, and contact information.</p>
+              <a href="https://github.com/Sikhuphela/Portfolio.git">
+                <FaGithub /> {/* GitHub icon */}
+              </a>
+            </div>
+          </div>
+        </div>
 
-<div className='group-4'>
-  <img className='Taxc' alt='Tax' src={tax} />
-  <div className='text-wrapper-3'>Tax Calculator</div>
-  <div className="work-3">
-    <div className="layer-3">
-      <p>User-friendly description of project 4.</p>
-      <a href="https://github.com/yourusername/project4.git">
-        <FaGithub /> 
-      </a>
-    </div>
-  </div>
-</div>
+        {/* Financial Calculator */}
+        <div className='calculator-3'>
+          <img className='calculator' alt='fac' src={FAC} />
+          <div className='text-wrapper-2'>Financial calculator</div>
+          <div className="work-2">
+            <div className="layer-2">
+              <p>Constructed using the Tkinter library in Python. Finance Calculator offers an intuitive interface for users to conduct investment and bond repayment calculations. This tool aids users in efficient financial management by providing a user-friendly platform for performing these calculations.</p>
+              <a href="https://github.com/Sikhuphela/Financial-Calculator.git">
+                <FaGithub /> 
+              </a>
+            </div>
+          </div>
+        </div>
+
+        {/* Tax Calculator */}
+        <div className='group-4'>
+          <img className='Taxc' alt='Tax' src={tax} />
+          <div className='text-wrapper-3'>Tax Calculator</div>
+          <div className="work-3">
+            <div className="layer-3">
+              <p>This software enables users to calculate their income tax for 2023 and 2024. It incorporates text fields, dropdowns, and a button for a smooth user-friendly experience. The calculated tax, rebate, and final amount are presented in a clear and succinct manner. This project demonstrates my proficiency in Java, Swing, and algorithmic problem-solving.</p>
+              <a href="https://github.com/Sikhuphela/Tax-Calculator.git">
+                <FaGithub /> 
+              </a>
+            </div>
+          </div>
+        </div>
 
       </div>
     </div>
