6164664f5612a22c1cf2e67b8a782e5f79cb3442
Sikhuphela <aphiwe.sikhuphela@younglings.africa>
2024-02-16 12:24:32 +0200

    Adde circles for Backkend skills progress circle

--- src/Pages/Skills.js
+++ src/Pages/Skills.js
@@ -44,6 +44,45 @@ function Skills() {
           </div>
         </div>
       </div>
+
+      <div className='container1'>
+      <h1>Backend Skills</h1>
+      <div className='radial-bars'>
+      <svg x ='0px' y='0px' viewBox='0 0 200 200'>
+      <circle className='progress-bar' cx='100' cy='100' r='80'></circle>
+      <circle className='path path-1' cx='100' cy='100' r='80'></circle>
+      </svg>
+      <div className='percentage'>70%</div>
+      <div className='text'>Java</div>
+      </div>
+
+      <div className='radial-bars'>
+      <svg x ='0px' y='0px' viewBox='0 0 200 200'>
+      <circle className='progress-bar' cx='100' cy='100' r='80'></circle>
+      <circle className='path path-1' cx='100' cy='100' r='80'></circle>
+      </svg>
+      <div className='percentage'>70%</div>
+      <div className='text'>Python</div>
+      </div>
+
+      <div className='radial-bars'>
+      <svg x ='0px' y='0px' viewBox='0 0 200 200'>
+      <circle className='progress-bar' cx='100' cy='100' r='80'></circle>
+      <circle className='path path-1' cx='100' cy='100' r='80'></circle>
+      </svg>
+      <div className='percentage'>70%</div>
+      <div className='text'>NodeJs</div>
+      </div>
+
+      <div className='radial-bars'>
+      <svg x ='0px' y='0px' viewBox='0 0 200 200'>
+      <circle className='progress-bar' cx='100' cy='100' r='80'></circle>
+      <circle className='path path-1' cx='100' cy='100' r='80'></circle>
+      </svg>
+      <div className='percentage'>70%</div>
+      <div className='text'>Java</div>
+      </div>
+      </div>
     </section>
   );
 }
