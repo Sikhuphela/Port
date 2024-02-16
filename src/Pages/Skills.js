f550f8c1d964ce618e109a24341291ac7e2c3b3a
Sikhuphela <aphiwe.sikhuphela@younglings.africa>
2024-02-16 15:20:32 +0200

    Added style for skill bars

--- src/Pages/Skills.js
+++ src/Pages/Skills.js
@@ -1,15 +1,15 @@
 import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faHtml5, faCss3Alt, faJs, faReact } from '@fortawesome/free-brands-svg-icons';
+import './Skills.css';
 
 function Skills() {
   return (
     <section>
-      <div className='skills'>
         <h3>My Skills</h3>
-
-        <h1 className='heading'>Frontend skills</h1>
-        <div className='Technical-bas'>
+        <div className='container1' id='Skills'>
+        <h1 className='heading'>Technical Skills</h1>
+        <div className='Technical-bars'>
           <div className='bar'><FontAwesomeIcon icon={faHtml5} style={{ color: '#f0edcf' }} />
             <div className='info'>
               <span>HTML</span>
@@ -46,7 +46,7 @@ function Skills() {
       </div>
 
       <div className='container1'>
-      <h1>Backend Skills</h1>
+      <h1 className='heading1'>Backend Skills</h1>
       <div className='radial-bars'>
       <svg x ='0px' y='0px' viewBox='0 0 200 200'>
       <circle className='progress-bar' cx='100' cy='100' r='80'></circle>
