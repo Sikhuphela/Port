66e084ce62014fb23d8f634c9d57798a003c0174
Sikhuphela <aphiwe.sikhuphela@younglings.africa>
2024-02-19 11:41:37 +0200

    added transition to circle bars

--- src/Pages/Skills.js
+++ src/Pages/Skills.js
@@ -6,11 +6,12 @@ import './Skills.css';
 function Skills() {
   return (
     <section>
-        <h3>My Skills</h3>
-        <div className='container1' id='Skills'>
+      <h3>My Skills</h3>
+      <div className='container1' id='Skills'>
         <h1 className='heading1'>Technical Skills</h1>
         <div className='Technical-bars'>
-          <div className='bar'><FontAwesomeIcon icon={faHtml5} style={{ color: '#f0edcf' }} />
+          <div className='bar'>
+            <FontAwesomeIcon icon={faHtml5} style={{ color: '#f0edcf' }} />
             <div className='info'>
               <span>HTML</span>
             </div>
@@ -18,7 +19,8 @@ function Skills() {
               <span></span>
             </div>
           </div>
-          <div className='bar'><FontAwesomeIcon icon={faCss3Alt}  style={{ color: '#f0edcf' }} /> 
+          <div className='bar'>
+            <FontAwesomeIcon icon={faCss3Alt} style={{ color: '#f0edcf' }} />
             <div className='info'>
               <span>CSS</span>
             </div>
@@ -26,7 +28,8 @@ function Skills() {
               <span></span>
             </div>
           </div>
-          <div className='bar'><FontAwesomeIcon icon={faJs}  style={{ color: '#f0edcf' }}  />
+          <div className='bar'>
+            <FontAwesomeIcon icon={faJs} style={{ color: '#f0edcf' }} />
             <div className='info'>
               <span> JavaScript</span>
             </div>
@@ -34,7 +37,8 @@ function Skills() {
               <span></span>
             </div>
           </div>
-          <div className='bar'><FontAwesomeIcon icon={faReact}  style={{ color: '#f0edcf' }} />
+          <div className='bar'>
+            <FontAwesomeIcon icon={faReact} style={{ color: '#f0edcf' }} />
             <div className='info'>
               <span> ReactJs</span>
             </div>
@@ -46,43 +50,44 @@ function Skills() {
       </div>
 
       <div className='container1'>
-      <h1 className='heading1'>Backend Skills</h1>
-      <div className='radial-bars'>
-      
-      <svg x ='0px' y='0px' viewBox='0 0 200 200'>
-      <circle className='progress-bar' cx='100' cy='100' r='80'></circle>
-      <circle className='path path-1' cx='100' cy='100' r='80'></circle>
-      </svg>
-      <div className='percentage'>70%</div>
-      <div className='text'>Java</div>
-      </div>
+        <h1 className='heading1'>Backend Skills</h1>
+        <div className='radial-bars'>
+          <div className='radial-bar'>
+            <svg viewBox='0 0 200 200'>
+              <circle className='progress-bar' cx='100' cy='100' r='80'></circle>
+              <circle className='path path-1' cx='100' cy='100' r='80'></circle>
+            </svg>
+            <div className='percentage'>70%</div>
+            <div className='text'>Java</div>
+          </div>
 
-      <div className='radial-bars'>
-      <svg x ='0px' y='0px' viewBox='0 0 200 200'>
-      <circle className='progress-bar' cx='100' cy='100' r='80'></circle>
-      <circle className='path path-1' cx='100' cy='100' r='80'></circle>
-      </svg>
-      <div className='percentage'>70%</div>
-      <div className='text'>Python</div>
-      </div>
+          <div className='radial-bar'>
+            <svg viewBox='0 0 200 200'>
+              <circle className='progress-bar' cx='100' cy='100' r='80'></circle>
+              <circle className='path path-1' cx='100' cy='100' r='80'></circle>
+            </svg>
+            <div className='percentage'>70%</div>
+            <div className='text'>Python</div>
+          </div>
 
-      <div className='radial-bars'>
-      <svg x ='0px' y='0px' viewBox='0 0 200 200'>
-      <circle className='progress-bar' cx='100' cy='100' r='80'></circle>
-      <circle className='path path-1' cx='100' cy='100' r='80'></circle>
-      </svg>
-      <div className='percentage'>70%</div>
-      <div className='text'>NodeJs</div>
-      </div>
+          <div className='radial-bar'>
+            <svg viewBox='0 0 200 200'>
+              <circle className='progress-bar' cx='100' cy='100' r='80'></circle>
+              <circle className='path path-1' cx='100' cy='100' r='80'></circle>
+            </svg>
+            <div className='percentage'>70%</div>
+            <div className='text'>NodeJs</div>
+          </div>
 
-      <div className='radial-bars'>
-      <svg x ='0px' y='0px' viewBox='0 0 200 200'>
-      <circle className='progress-bar' cx='100' cy='100' r='80'></circle>
-      <circle className='path path-1' cx='100' cy='100' r='80'></circle>
-      </svg>
-      <div className='percentage'>70%</div>
-      <div className='text'>Java</div>
-      </div>
+          <div className='radial-bar'>
+            <svg viewBox='0 0 200 200'>
+              <circle className='progress-bar' cx='100' cy='100' r='80'></circle>
+              <circle className='path path-1' cx='100' cy='100' r='80'></circle>
+            </svg>
+            <div className='percentage'>70%</div>
+            <div className='text'>Java</div>
+          </div>
+        </div>
       </div>
     </section>
   );
