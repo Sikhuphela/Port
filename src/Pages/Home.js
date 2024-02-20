1a0bfabfdda7b6c90e90dc6fc39b9d2102f0d6d8
Sikhuphela <aphiwe.sikhuphela@younglings.africa>
2024-02-20 10:18:31 +0200

    changed the h3 name to h2 and move the name under the image

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -6,7 +6,7 @@ import picImage from '../../src/components/Images/pic.jpg';
 function Home() {
   return (
     <div className='home'>
-      <h3> APHIWE SIKHUPHELA</h3>
+      <h2> APHIWE SIKHUPHELA</h2>
       <h4>Software development</h4>
       <div className="image">
         <img className="pic" alt="Pic" src={picImage} />
