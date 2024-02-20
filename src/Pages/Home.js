64625d37985e36a987c066b2c3636425f08144f9
Sikhuphela <aphiwe.sikhuphela@younglings.africa>
2024-02-20 11:11:58 +0200

    Added an about me button

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,17 +1,21 @@
 import React from 'react';
-import './style.css';
-import picImage from '../../src/components/Images/pic2.jpg';
-
+import { Link } from 'react-router-dom'; 
+import './Home.css'; 
+import picImage from './pic.jpg';
 
 function Home() {
   return (
     <div className='home'>
-    <p>Hello!  My Name Is</p>
-      <h2> APHIWE SIKHUPHELA</h2>
-      <h4>  Software development</h4>
+      <p>Hello! My Name Is</p>
+      <h2>APHIWE SIKHUPHELA</h2>
+      <h4>A <br />Software Development</h4> 
       <div className="image">
-        <img className="pic" alt="Pic" src={picImage} />
+        <img className="pic" alt="Pic" src={picImage} /> 
       </div>
+      
+      <Link to="/about">
+        <button>ABOUT ME</button>
+      </Link>
     </div>
   );
 }
