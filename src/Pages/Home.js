e65fdc358a395825e794c3ccd65997469da36fb9
Sikhuphela <aphiwe.sikhuphela@younglings.africa>
2024-02-20 11:15:45 +0200

    added a link to about me me on the button

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,18 +1,18 @@
 import React from 'react';
+import './style.css';
 import { Link } from 'react-router-dom'; 
-import './Home.css'; 
-import picImage from './pic.jpg';
+import picImage from '../../src/components/Images/pic2.jpg';
+
 
 function Home() {
   return (
     <div className='home'>
-      <p>Hello! My Name Is</p>
-      <h2>APHIWE SIKHUPHELA</h2>
-      <h4>A <br />Software Development</h4> 
+    <p>Hello!  My Name Is</p>
+      <h2> APHIWE SIKHUPHELA</h2>
+      <h4>  Software development</h4>
       <div className="image">
-        <img className="pic" alt="Pic" src={picImage} /> 
+        <img className="pic" alt="Pic" src={picImage} />
       </div>
-      
       <Link to="/about">
         <button>ABOUT ME</button>
       </Link>
