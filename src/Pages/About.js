9e216a6984aab1533df743ad28bf0e6dd8a46db8
Sikhuphela <aphiwe.sikhuphela@younglings.africa>
2024-03-18 16:43:59 +0200

    updated about page

--- src/Pages/About.js
+++ src/Pages/About.js
@@ -1,13 +1,13 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './About.css'; 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faBook, faUtensils, faCode, faGlobe } from '@fortawesome/free-solid-svg-icons';
 import picImage1 from '../../src/components/Images/pic.jpg';
 import cv from '../../src/Pages/CV/Aphiwe_Sikhuphela_Professional_CV.pdf.pdf';
 
-
-
 function About() {
+  const [activeTab, setActiveTab] = useState('skills');
+
   const downloadCV = () => {
     const link = document.createElement('a');
     link.href = cv;
@@ -16,6 +16,11 @@ function About() {
     link.click();
     document.body.removeChild(link);
   };
+
+  const opentab = (tabname) => {
+    setActiveTab(tabname);
+  };
+
   return (
     <div className='about-container'>
       <h3>About Me</h3>
@@ -28,24 +33,15 @@ function About() {
               <img className="IMG" alt="Img" src={picImage1} />
             </div>
           </div>
-          <div className="div">Hobbies</div>
-          <div className="overlap-group">
-            <div className="text-wrapper-2"><FontAwesomeIcon icon={faBook} style={{ color: "#f0edcf" }} /> Reading</div>
-            <div className="cooking"><FontAwesomeIcon icon={faUtensils} style={{ color: "#f0edcf" }} /> Cooking</div>
-          </div>
-          <div className="text-wrapper-3">
-            <FontAwesomeIcon icon={faCode} style={{ color: "#f0edcf" }} />  Coding
-          </div>
-          <div className="text-wrapper-5">
-            <FontAwesomeIcon icon={faGlobe} style={{ color: "#f0edcf" }} /> Culture Studies
-          </div>
-          <div className="group-2"></div>
-          <div className="text-wrapper-7">Interests</div>
           <div className="group-wrapper">
             <div className="div-wrapper">
               <div className="card">
                 <div className="card__title">
-                  <p className="small-desc">Energetic Software Developer Intern currently interning at ABSA, with a strong background in a software development learning program at Uvu Africa. Proven track record in meeting deadlines and delivering outstanding results. Demonstrated leadership as Workshop Coordinator for the Second African Computer and Human Interactions Conference in 2018. Completed National Diploma for Information Technology at Walter Sisulu University.</p>
+                  <p className="small-desc">Energetic Software Developer Intern currently interning at ABSA, 
+                  with a strong background in a software development learning program at Uvu Africa.
+                   Proven track record in meeting deadlines and delivering outstanding results. 
+                   Demonstrated leadership as Workshop Coordinator for the Second African Computer and Human Interactions Conference in 2018.
+                    Completed National Diploma for Information Technology at Walter Sisulu University.</p>
                   <div className="go-corner">
                     <div className="go-arrow">+</div>
                   </div>
@@ -53,13 +49,38 @@ function About() {
               </div>
             </div>
           </div>
+          <div className="tab-container">
+          <div className="tab-titles">
+            <p className={`tab-links ${activeTab === 'skills' ? 'active-link' : ''}`} onClick={() => opentab('skills')}>Skills</p>
+            <p className={`tab-links ${activeTab === 'experience' ? 'active-link' : ''}`} onClick={() => opentab('experience')}>Experience</p>
+            <p className={`tab-links ${activeTab === 'education' ? 'active-link' : ''}`} onClick={() => opentab('education')}>Education</p>
+          </div>
+          <div className={`tab-contents ${activeTab === 'skills' ? 'active-tab' : ''}`} id='skills'>
+            <ul>
+              <li><span>UI/UX</span><br />Designing Web/App interfaces</li>
+              <li><span>Web Development</span><br /> Web App Development</li>
+              <li><span>App development</span><br />Building android/iOS Apps</li>
+            </ul>
+          </div>
+          <div className={`tab-contents ${activeTab === 'experience' ? 'active-tab' : ''}`} id='experience'>
+            <ul>
+              <li><span>2024-Current</span><br />Software Developer at ABSA</li>
+              <li><span>2023-2024</span><br /> Software Developer Intern at UVU Africa</li>
+            </ul>
+          </div>
+          <div className={`tab-contents ${activeTab === 'education' ? 'active-tab' : ''}`} id='education'>
+            <ul>
+              <li><span>2019</span><br />NDip Information Technology at<br/> Walter Sisulu University</li>
+              <li><span>2013</span><br /> Matric at Sundra Secondary School</li>
+            </ul>
+          </div>
           <div>
-          <button className="btn" onClick={downloadCV}>Download CV</button>
-        </div>
+            <button className="btn" onClick={downloadCV}>Download CV</button>
+          </div>
         </div>
       </div>
     </div>
-    
+    </div>
   );
 }
 
--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -6,9 +6,11 @@ import picImage from '../../src/components/Images/pic2.jpg';
 function Home() {
   return (
     <div className='home'>
+    <div content='homes'>
       <p>Hello! My Name Is</p>
       <h2>APHIWE SIKHUPHELA</h2>
       <h4>Software Development</h4>
+      </div>
       <div className="image">
         <img className="pic" alt="Pic" src={picImage} />
       </div>
